9459193376d205d940c5149918c1c7ef80c2b0e9
agu <25160294+aeu79@users.noreply.github.com>
2020-03-28 11:55:47 +0100

    Minor code style improvements to PMIs

--- pmis.py
+++ pmis.py
@@ -1,122 +1,125 @@
+import math
+import time
+from itertools import product
+
 import numpy as np
 import scipy.spatial as scispa
-import os,sys, math
-from itertools import product
-import time
-def compute_pmis(encoded_focus_alignment, pseudocount_weight, theta):
 
+
+def compute_pmis(encoded_focus_alignment, pseudocount_weight, theta):
     encoded_focus_alignment = encoded_focus_alignment.astype(int)
 
-    q= 21
-#compute the empirical frequencies from the sequence data
+    q = 21
+    # compute the empirical frequencies from the sequence data
     time1 = time.time()
-    Meff, Pij_true, Pi_true, alignment_width = count_alignment(encoded_focus_alignment, theta, q)
+    meff, Pij_true, Pi_true, alignment_width = count_alignment(encoded_focus_alignment, theta, q)
     time2 = time.time()
     print("count alignment function takes: " + str(time2 - time1))
 
-    #include pseudocounts
-    Pij,Pi= with_pc(Pij_true, Pi_true, pseudocount_weight, alignment_width, q)
+    # include pseudocounts
+    Pij, Pi = with_pc(Pij_true, Pi_true, pseudocount_weight, alignment_width, q)
     time3 = time.time()
     print("with_pc function takes: " + str(time3 - time2))
-#compute PMIs
+    # compute PMIs
     PMIs = Get_PMIs(Pij, Pi, alignment_width, q)
     time4 = time.time()
     print("get_PMIs function takes: " + str(time4 - time3))
-    #np.save('PMIsssfile.npy',PMIs)
-    return PMIs, Meff
+    # np.save('PMIsssfile.npy',PMIs)
+    return PMIs, meff
 
 
 ##auxiliary function definitions
 def count_alignment(encoded_focus_alignment, theta, q):
-#calculate Meff
+    # calculate Meff
     alignment_height = encoded_focus_alignment.shape[0]
     alignment_width = encoded_focus_alignment.shape[1]
     W = np.ones((1, alignment_height))
-#whether you weight or not
+    # whether you weight or not
     if theta > 0.0:
-        encoded_focus_alignment_pdist=scispa.distance.pdist(encoded_focus_alignment,'hamming')
-        booleanTF= theta > encoded_focus_alignment_pdist
-        boolean01_encoded_focus= booleanTF.astype(int)
-        W = (1./(1+sum(scispa.distance.squareform(boolean01_encoded_focus))))
-    Meff=sum(W)
-#compute the frequencies
+        encoded_focus_alignment_pdist = scispa.distance.pdist(encoded_focus_alignment, 'hamming')
+        booleanTF = theta > encoded_focus_alignment_pdist
+        boolean01_encoded_focus = booleanTF.astype(int)
+        W = (1. / (1 + sum(scispa.distance.squareform(boolean01_encoded_focus))))
+    Meff = sum(W)
+    # compute the frequencies
     Pij_true = np.zeros((alignment_width, alignment_width, q, q))
     Pi_true = np.zeros((alignment_width, q))
-#single-site frequencies
+
+    # single-site frequencies
 
     def vectorize(data):
         result = np.zeros((q,))
         for i in range(q):
-            result[i] = W[data==i].sum()
+            result[i] = W[data == i].sum()
         return result
 
-    Pi_true = np.apply_along_axis(vectorize , 0 , encoded_focus_alignment)
+    Pi_true = np.apply_along_axis(vectorize, 0, encoded_focus_alignment)
     Pi_true = Pi_true.T
 
+    # normalization
+    Pi_true = Pi_true / Meff
 
-#normalization
-    Pi_true = Pi_true/Meff
+    # two-site frequencies
 
-#two-site frequencies
-
-    Pij_ind = np.triu_indices(alignment_width,1)
+    Pij_ind = np.triu_indices(alignment_width, 1)
 
     for k in range(len(Pij_ind[0])):
-        e,j = Pij_ind[0][k], Pij_ind[1][k]
+        e, j = Pij_ind[0][k], Pij_ind[1][k]
         e_col = encoded_focus_alignment[:, e]
         j_col = encoded_focus_alignment[:, j]
         np.add.at(Pij_true[e, j, :, :], [e_col.tolist(), j_col.tolist()], W)
         Pij_true[j, e, :, :] = Pij_true[e, j, :, :].T
 
+    Pij_true = Pij_true / Meff
 
-    Pij_true = Pij_true/Meff
-
-    #fill the diagonal of Pij_true
-    diag_ind = product(list(range(alignment_width)),list(range(q)))
+    # fill the diagonal of Pij_true
+    diag_ind = product(list(range(alignment_width)), list(range(q)))
     for k in diag_ind:
-        Pij_true[k[0],k[0],k[1],k[1]]=Pi_true[k[0],k[1]]
+        Pij_true[k[0], k[0], k[1], k[1]] = Pi_true[k[0], k[1]]
+
+    return (Meff, Pij_true, Pi_true, alignment_width)
 
 
-
-    return(Meff, Pij_true, Pi_true, alignment_width)
 def with_pc(Pij_true, Pi_true, pseudocount_weight, alignment_width, q):
-#add pseudocounts to deal with some finite size effects
-    Pij = (1.-pseudocount_weight)*Pij_true + pseudocount_weight/q/q*(np.ones((alignment_width, alignment_width, q, q)))
-    Pi = (1.-pseudocount_weight)*Pi_true + pseudocount_weight/q*(np.ones((alignment_width, q)))
-#correct things on the diagonal
+    # add pseudocounts to deal with some finite size effects
+    Pij = (1. - pseudocount_weight) * Pij_true + pseudocount_weight / q / q * (
+        np.ones((alignment_width, alignment_width, q, q)))
+    Pi = (1. - pseudocount_weight) * Pi_true + pseudocount_weight / q * (np.ones((alignment_width, q)))
+    # correct things on the diagonal
     # scra = np.identity(q)
     # for i in range(alignment_width):
     #     for alpha in range(1,q):
     #         for beta in range(1,q):
     #             Pij[i, i, alpha, beta] = (1.-pseudocount_weight)*Pij_true[i, i, alpha, beta] + pseudocount_weight/q*scra[alpha, beta]
     #
-    psedo_ind = product(list(range(alignment_width)),list(range(1,q)))
+    psedo_ind = product(list(range(alignment_width)), list(range(1, q)))
     for k in psedo_ind:
-        Pij[k[0], k[0], k[1], k[1]] = (1. - pseudocount_weight) * Pij_true[k[0], k[0], k[1], k[1]] + pseudocount_weight / q
+        Pij[k[0], k[0], k[1], k[1]] = (1. - pseudocount_weight) * Pij_true[
+            k[0], k[0], k[1], k[1]] + pseudocount_weight / q
 
     ##keep non-diagnal elements zero
     for l in range(alignment_width):
-            Pij[l, l, 1:, 1:][np.triu_indices(q - 1, 1)] = 0
-            Pij[l, l, 1:, 1:][np.triu_indices(q - 1, 1)[::-1]] = 0
+        Pij[l, l, 1:, 1:][np.triu_indices(q - 1, 1)] = 0
+        Pij[l, l, 1:, 1:][np.triu_indices(q - 1, 1)[::-1]] = 0
 
-    return(Pij, Pi)
+    return (Pij, Pi)
+
 
 def Get_PMIs(Pij, Pi, alignment_width, q):
-#Compute PMIs from the matrices of frequencies.
-#Here, the 1st aa type(=gap) is ignored (slight improvement)
-    C=np.zeros((alignment_width,alignment_width,q,q))
+    # Compute PMIs from the matrices of frequencies.
+    # Here, the 1st aa type(=gap) is ignored (slight improvement)
+    C = np.zeros((alignment_width, alignment_width, q, q))
 
-## my code
-    C_ind = np.triu_indices(alignment_width,0)
+    ## my code
+    C_ind = np.triu_indices(alignment_width, 0)
     for m in range(len(C_ind[0])):
-        i,j=C_ind[0][m], C_ind[1][m]
-        Pi_mat = np.dot(Pi[i,:][1:,None],Pi[j,:][None,1:])
-        C[i,j,1:,1:]= np.log(Pij[i,j,1:,1:]/Pi_mat)
-        C[j,i,1:,1:]= np.log(Pij[j,i,1:,1:]/Pi_mat.T)
+        i, j = C_ind[0][m], C_ind[1][m]
+        Pi_mat = np.dot(Pi[i, :][1:, None], Pi[j, :][None, 1:])
+        C[i, j, 1:, 1:] = np.log(Pij[i, j, 1:, 1:] / Pi_mat)
+        C[j, i, 1:, 1:] = np.log(Pij[j, i, 1:, 1:] / Pi_mat.T)
 
-    #mask for Pij=0
-    C[ Pij == 0 ] = -math.inf
-
+    # mask for Pij=0
+    C[Pij == 0] = -math.inf
 
     # for i in range(alignment_width):
     #     for j in range(alignment_width):
@@ -129,9 +132,10 @@ def Get_PMIs(Pij, Pi, alignment_width, q):
 
     return C
 
+
 if __name__ == "__main__":
-    fi=np.load('encoded_training_alignment.npy')
-    #fi= os.sys.argv[1]
-    pseudocount_weight=0.15
-    theta=0.15
+    fi = np.load('encoded_training_alignment.npy')
+    # fi= os.sys.argv[1]
+    pseudocount_weight = 0.15
+    theta = 0.15
     compute_pmis(fi, pseudocount_weight, theta)
