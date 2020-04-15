11f58dde497bb3a1d32ad2c92b23f2c6f899dc7a
Agustín <25160294+aeu79@users.noreply.github.com>
2020-04-15 13:37:27 +0200

    Complete profiler and filter stats.

--- testing.py
+++ testing.py
@@ -1,37 +1,42 @@
-import unittest
-
-
 """
     Unit testing for the MI_IPA python implementation.
 
     Usage:
-    import testing
-    test(all)    #Performs all the tests
-    testx()     # Test x
-    profiler()   # Makes a profile with the sample data.
+    python3 testing.py      # Performs all the tests
 
-    TODO:
+    Individual tests:
+    import testing
+    testing.test_x()      # Test x
+    
+    Profiler:
+    import testing
+    testing.profiler()   # Makes a profile with the sample data (default).
 """
 
+import unittest
 
-def profiler():
+
+# noinspection SpellCheckingInspection
+def profiler(output_stats='profile_stats'):
     import cProfile
-    import mi_ipa_main
-    cProfile.run('mi_ipa_main.main()', 'profile_stats','tottime')
+    # import mi_ipa_main # Using this import and the following command crashes cProfile (report?):
+    # cProfile.run(mi_ipa_main.main(), 'profile_stats', 'tottime')
+    import pstats
 
-class TestMiipa(unittest.TestCase):
+    cProfile.run('mi_ipa_main.main()', 'profile_stats', 'tottime')
+    profile_results = pstats.Stats(output_stats).strip_dirs()
+    profile_results.sort_stats('tottime').print_stats(10)
+    print("You can now obtain a graph using: gprof2dot -f pstats profile_stats | dot -Tsvg -o " + output_stats + ".svg")
+
+
+class TestMiIpa(unittest.TestCase):
 
     def test_1(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
 
     def test_2(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
 
 
-
-# if __name__ == "__main__":
-#     testall()
-
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
