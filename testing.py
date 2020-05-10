8914ac7e60d3b6f5c12b973bc4ea415a02f7c4e1
agu <25160294+aeu79@users.noreply.github.com>
2020-05-10 06:13:37 +0200

    Add test to compare imported csv species list

--- testing.py
+++ testing.py
@@ -37,8 +37,23 @@ def profiler(output_stats='profile_stats'):
 
 class TestMiIpa(unittest.TestCase):
 
-    def test_1(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    def test_csv_species_list(self):
+        """Comparison of imported species from .mat and csv files."""
+        # from matlab data to df
+        import scipy.io as spio
+        import pandas as pd
+        # Read the matlab file:
+        self.mat = spio.loadmat('testing/SpeciesNumbering_Standard_HKRR_dataset.mat', squeeze_me=True)
+        self.col1 = list(range(len(self.mat['SpeciesNumbering_extr'])))
+        self.col2 = [x[1] for x in self.mat['SpeciesNumbering_extr']]
+        self.SpeciesNumbering_mat = pd.DataFrame([*zip(self.col1, self.col2)])
+        self.SpeciesNumbering_mat = self.SpeciesNumbering_mat.values.tolist() # Panda's DataFrame to list
+        # Now import the csv file:
+        self.species_list = 'testing/SpeciesNumbering_Standard_HKRR_dataset.csv'
+        self.SpeciesNumbering_csv = pd.read_csv(self.species_list, header=None, delimiter=',')
+        self.SpeciesNumbering_csv = self.SpeciesNumbering_csv.values.tolist()
+        #Only_unique_species_1col = self.SpeciesNumbering_csv[1].unique().tolist()
+        self.assertListEqual(self.SpeciesNumbering_mat, self.SpeciesNumbering_csv, "The list of species should be the same from .mat or .csv.")
 
     def test_2(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
@@ -46,3 +61,4 @@ class TestMiIpa(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
